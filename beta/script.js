d59833078bfaa6428d5b92090e6482eafb28df6a
eban_dev <eban@eban.home>
2020-10-23 11:02:18 +0200

    Lateral menu animation is working :

--- beta/script.js
+++ beta/script.js
@@ -2,48 +2,50 @@ var tran = new Translater();
 var is_opened = false;
 var pane = document.getElementById("pane");
 var menu_icon = document.getElementById("menu_icon")
+var page = document.getElementById("page")
+var body = document.getElementById("body")
 
 menu_icon.onclick = function() {
-  console.log(is_opened);
-
   if (is_opened) {
-    setTimeout(function () {
-      pane.style.width = "0%";
-      pane.style.opacity = '0';
-    }, 30);
-    menu_icon.style.marginRight = '50px';
-    is_opened = false;
+    closeNav();
   } else {
-    pane.style.display = 'block';
-    if (window.innerWidth < 800) {
-      menu_icon.style.display = 'none';
-      setTimeout(function () {
-        pane.style.width = "70%";
-        pane.style.opacity = '1';
-      }, 30);
-    } else {
-      menu_icon.style.marginRight = '23%';
-      setTimeout(function () {
-        pane.style.width = "20%";
-        pane.style.opacity = '1';
-      }, 30);
-    }
-    is_opened = true;
+    openNav();
   }
 };
 
-document.getElementById("page").onclick = function() {
-  setTimeout(function () {
-    pane.style.width = "0%";
-    pane.style.opacity = '0';
-  }, 3000);
-  pane.style.display = 'none';
-  menu_icon.style.display = 'block';
-  menu_icon.style.marginRight = '50px';
-  is_opened = false;
+page.onclick = function() {
+  if (is_opened) {
+    closeNav();
+  }
 };
 
-if (tran.getLang() ==="eo") {
+function openNav() {
+  if (window.innerWidth < 800) {
+    menu_icon.style.display = 'none';
+    pane.style.width = "70%";
+  } else {
+    pane.style.width = "20%";
+    menu_icon.style.marginRight = "calc(20% + 50px)";
+  }
+  setTimeout(function () {
+    pane.style.boxShadow = "10px 10px 5px 1500px rgba(0,0,0,0.5)";
+  }, 300);
+  body.style.overflowY = "hidden";
+  is_opened = true;
+}
+
+function closeNav() {
+  pane.style.width = "0";
+  setTimeout(function () {
+    pane.style.boxShadow = "0px 0px 0px 0px rgba(0,0,0,0)";
+  }, 300);
+  menu_icon.style.marginRight = "50px";
+  body.style.overflowY = "auto";
+  menu_icon.style.display = 'block';
+  is_opened = false;
+}
+
+if (tran.getLang() === "eo") {
   var all = document.getElementsByClassName('btn');
   for (var i = 0; i < all.length; i++) {
     all[i].style.width = '91px';
@@ -57,14 +59,17 @@ if (tran.getLang() ==="eo") {
 
 document.getElementById("en").onclick = function() {
   tran.setLang('default');
+  closeNav();
 };
 
 document.getElementById("fr").onclick = function() {
   tran.setLang('fr');
+  closeNav();
 };
 
 document.getElementById("eo").onclick = function() {
   tran.setLang('eo');
+  closeNav();
 };
 
 var userLang = navigator.language || navigator.userLanguage;
